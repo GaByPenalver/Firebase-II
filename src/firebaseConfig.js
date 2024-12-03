c6ac627773a902c88a62bd033769f060a9197818
GaBy Peñalver <Gabypealver14@gmail.com>
2024-12-03 12:56:18 -0300

    change

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyADzfjXsCY9V8Nw0PNJCemt_nVkJzz6L6Y",
+  authDomain: "fir-auth-f09e3.firebaseapp.com",
+  projectId: "fir-auth-f09e3",
+  storageBucket: "fir-auth-f09e3.firebasestorage.app",
+  messagingSenderId: "1083958038516",
+  appId: "1:1083958038516:web:89201f63a0b8a5277233a1"
 };
 
 const app = initializeApp(firebaseConfig);
